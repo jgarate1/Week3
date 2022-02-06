0c3c79fa1e61b7837bc85367e0e08f294cb6f41e
jgarate1 <garatejustus@gmail.com>
2022-02-06 14:41:45 -0600

    added pending/complete and input box

--- vitetodoapp/src/App.vue
+++ vitetodoapp/src/App.vue
@@ -2,7 +2,24 @@
 </script>
 
 <template>
-<h1 class="font-thin text-6xl">
-  Hello There
+<h1 class="font-thin text-6xl tracking-tight">
+ 🚀 My Todo App
 </h1>
+<input type="text" class="border border-black"/>
+<div>
+<div>
+  <h3>Pending</h3>
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+  </ul>
+</div>
+<div>
+  <h3>Completed</h3>
+  <ul>
+    <li>Three</li>
+  </ul>
+
+</div>
+</div>
 </template>
