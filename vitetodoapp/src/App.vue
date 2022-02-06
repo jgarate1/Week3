9f834b490c3fdfcf3242526068864562c3cd0440
jgarate1 <garatejustus@gmail.com>
2022-02-06 15:50:04 -0600

    Added Pending and Completed Changes

--- vitetodoapp/src/App.vue
+++ vitetodoapp/src/App.vue
@@ -1,33 +1,71 @@
 <script setup>
-import {ref} from "vue";
+import {ref, computed } from "vue";
+const newTodo = ref("");
 
-const todos=ref([
-  {id:0, content:"hello", done:false},
-  {id:1, content:"world", done:false},
-  {id:2, content: "Hi", done: true},
-  
-  ])
+const todos=ref([]);
+
+const pending = computed(() => {
+  return todos.value.filter((todo) => !todo.done);
+});
+
+const completed = computed(() => {
+  return todos.value.filter((todo) => todo.done);
+});
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    todos.value.push({
+      id: todos.value.length,
+      content: newTodo.value,
+      done: false,
+    });
+    newTodo.value = "";
+  }
+};
+
+const changeStatus = (id) => {
+  const todo = todos.value.find((todo) => todo.id === id);
+  todo.done = !todo.done;
+};
 </script>
 
 <template>
 <div class="min-h-screen bg-gray-800">
   <div class="container mx-auto flex flex-col space-y-8 pt-8">  
-    <h1 class="font-thin text-center text-6xl tracking-tight text-gray-300">
+    <h1 class="pb-4 font-thin text-center text-6xl tracking-tight text-gray-300">
       🚀 My Todo App
     </h1>
-    <input type="text" class="rounded-lg text-center px-4 py-2 text-xl"
-    placeholder="New Todo"/>
-    <div class="flex justify-between">
+    <input
+        @change="addTodo"
+        v-model="newTodo"
+        type="text"
+        class="px-4 py-2 text-xl text-center rounded-lg"
+        placeholder="New Todo"
+      />
+    <div class="flex justify-around">
       <div class="w-1/3">
         <h3 class="text-2xl text-center text-blue-400">Pending</h3>
           <ul class="pt-8 space-y-4">
-            <li v-for="todo in todos" :key="todo.id" class="font-bold text-blue-600 bg-gray-300 w-full text-center px-4 py-2 rounded-lg hover:curser-pointer hover:bg-blue-400 hover:text-gray-200 duration-500">{{todo.content}}</li>
+            <li
+              v-for="todo in pending"
+              :key="todo.id"
+              @click="changeStatus(todo.id)"
+              class="w-full px-4 py-2 font-bold text-center text-blue-600 transition-colors duration-500 bg-gray-300 rounded-lg  hover:cursor-pointer hover:bg-blue-400 hover:text-gray-200"
+            >
+              {{ todo.content }}
+            </li>
           </ul>
       </div>
       <div class="w-1/3">
         <h3 class="text-2xl text-center text-green-400">Completed</h3>
           <ul class="pt-8 space-y-4">
-            <li v-for="todo in todos" :key="todo.id" class="font-bold text-green-600 bg-gray-300 w-full text-center px-4 py-2 rounded-lg hover:curser-pointer hover:bg-green-400 hover:text-gray-200 duration-500">{{todo.content}}</li>
+             <li
+              v-for="todo in completed"
+              :key="todo.id"
+              @click="changeStatus(todo.id)"
+              class="w-full px-4 py-2 font-bold text-center text-green-600 transition-colors duration-500 bg-gray-300 rounded-lg  hover:cursor-pointer hover:bg-green-400 hover:text-gray-200"
+            >
+              {{ todo.content }}
+            </li>
           </ul>
 
       </div>
